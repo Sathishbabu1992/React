d5f6f70475f90f086db17f94c11f29c2c5d8eb22
unknown <Sathishbabu1992>
2024-02-24 08:28:48 +0000

    myFirst file

---
+++ js/script.js
@@ -0,0 +1,34 @@
+let a =[1, "St", true, {a:11, b:12}, function(){console.log('hi')}, undefined, , ["Saab", "Volvo", "BMW"], {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"}]
+
+console.log(a[0]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+a[4]();
+console.log(a[5]);
+console.log(a[6]);
+console.log(a[7][0]);
+console.log(a[7]);
+console.log(a[8]);
+console.log(a[9]);
+
+console.log("*******************************************************************");
+
+
+
+
+
+
+
+
+ let b={name:"sathis", age:24, degree:true, s:function(){console.log('hi')}, arr:["Saab", "Volvo", "BMW"], pers:{firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"}
+ }
+
+ console.log(b.name);
+ console.log(b.age);
+ console.log(b.degree);
+ console.log(b.s);
+ b.s();
+ console.log(b.arr[0]);
+ console.log(b.pers.firstName);
+
